75736cee9c73bd819a8268fe8ff4fe0be28b5c1b
humy06001 <humy06001@E-PW0234KS>
2023-10-18 13:11:18 +0200

    Add project files.

---
+++ Kap 4 Uppgift 4.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentNr = 0;
+            int newNr = 0;
+
+            Console.WriteLine("Skriv en ett heltal:");
+            string userNr = Console.ReadLine();
+
+            for (int i = 0; i < userNr.Length; i++) 
+            {
+                currentNr = int.Parse(userNr[i].ToString()); //Aktuella siffran
+
+                newNr = currentNr == 9 ? currentNr - 1 : currentNr + 1; //Ny siffra
+                
+                if (i == 0) //första loopen
+                {
+                    Console.WriteLine("Här är ditt nya tal:");
+                }
+
+                Console.Write(newNr); //skriv ut ny siffra
+            }
+        }
+    }
+}
